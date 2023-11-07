30c18da51d6533aafd17830d86973cb73dfb3365
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-07 18:17:33 +0530

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import UserRouter from "./routes/user.route.js";
 dotenv.config();
 
-try {
-  mongoose.connect(process.env.MONGO);
-  console.log("Connected to MongoDB");
-} catch (error) {
-  console.error("Error connecting to MongoDB:", error);
-}
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 const app = express();
 const port = 3000;
+
+app.use("/api/user", UserRouter);
 
 app.listen(port, () => {
   console.log(` app listening on port ${port}!!`);
