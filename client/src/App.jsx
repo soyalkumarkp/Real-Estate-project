769bc9258e85a55f2eb0796d4497cb9504c1719f
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-07 09:29:26 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import UpdateListing from "./pages/UpdateListing";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/update-listing" element={<UpdateListing />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
