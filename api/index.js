4213fbae4ad7815828f88ad2e4bb67d10d92efa7
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-07 16:19:59 +0530

    Revert "Revert "Connect to database""
    This reverts commit 13ae7dcc53f71bd5b8a5f77fd2bb5d8b050207d2.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+try {
+  mongoose.connect(process.env.MONGO);
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
 const app = express();
 const port = 3000;
 
